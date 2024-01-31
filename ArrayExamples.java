07341e3900c45baa71109df80cdde278a4c3879c
annwon603 <aniwong603@gmail.com>
2024-01-31 15:05:25 -0800

    Fixed bugs
    1. Add test cases to find bugs
    2. Resolved the bugs in the ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,12 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] tempArray = new int[arr.length];
+    for(int i = 0; i < arr.length; i++){
+      tempArray[i] = arr[i];
+    }
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = tempArray[arr.length - i - 1];
     }
   }
 
@@ -13,10 +17,20 @@ public class ArrayExamples {
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
-    }
+    //if there is only one element
+    if(arr.length == 1){
+      return arr;
+    //otherwise
+    }else{
+      for(int i = 0 ; i < arr.length; i++){
+        newArray[i] = arr[i];
+      }
+      for(int i = 0; i < arr.length; i += 1) {
+        arr[i] = newArray[arr.length - i - 1];
+      }
     return arr;
+    }
+   
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -7,6 +10,12 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input2 = { 2 , 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 3, 2},input2);
+    int[] input3 = { 1 , 2 , 3};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{ 3, 2, 1},input3);
 	}
 
 
@@ -14,5 +23,16 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input2 = {3};
+    ArrayExamples.reversed(input2);
+    assertArrayEquals(new int[]{3}, input2);
+    int[] input3 = {2, 3};
+    ArrayExamples.reversed(input3);
+    assertArrayEquals(new int[]{3,2}, input3);
+    int[] input4 = {1,2,3};
+    ArrayExamples.reversed(input4);
+    assertArrayEquals(new int[]{3,2,1}, input4);
   }
+
+
 }
